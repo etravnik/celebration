c5520dbfe17eb5229d3079dccbc3c041dc8b0be8
Elizabeth Travnik <etravnik@nd.edu>
2021-03-19 12:06:35 -0400

    creating screens

---
+++ celebration/EditContact.swift
@@ -0,0 +1,29 @@
+//
+//  EditContact.swift
+//  celebration
+//
+//  Created by Elizabeth Travnik on 3/19/21.
+//
+
+import UIKit
+
+class EditContact: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

4773bf54a727ff68fdbb8148f3caf937d3112e4a
ckuse <officialconnorkuse@gmail.com>
2021-04-29 11:09:03 -0400

    completed birthday algorithm for main page

--- celebration/Home.swift
+++ celebration/Home.swift
@@ -9,6 +9,8 @@ import UIKit
 import Contacts
 
 class Home: UIViewController {
+    
+    let Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     let contact = CNMutableContact();
     let store = CNContactStore();
@@ -16,6 +18,8 @@ class Home: UIViewController {
     
     @IBOutlet weak var contactNameOne: UILabel!
     @IBOutlet weak var contactNameTwo: UILabel!
+    @IBOutlet weak var birthdayOne: UILabel!
+    @IBOutlet weak var birthdayTwo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,12 +48,48 @@ class Home: UIViewController {
             }
             
             // filter array of contacts based on who has a birthday
-            let bday_contacts = big_contacts.filter { $0.birthday?.day != nil }
+            var bday_contacts = big_contacts.filter { $0.birthday?.day != nil }
+            
+            let currentDate = Date()
+            let calendar = Calendar.current
+            
+            /*print(calendar.component(.year, from: currentDate))
+            print(calendar.component(.month, from: currentDate))
+            print(calendar.component(.day, from: currentDate))*/
+            
+            
+            // sort contacts by birthday in order, ignoring year (should work)
+            bday_contacts.sort { ($0.birthday?.day)! < ($1.birthday?.day)! }
+            bday_contacts.sort { ($0.birthday?.month)! < ($1.birthday?.month)! }
+            
             for contact in bday_contacts {
                 print("oh boy here we go: \(String(describing: contact.familyName)) \(String(describing: contact.birthday?.year))");
             }
             
-            //print("oh boy here we go: \(String(describing: contactsTwo[0].familyName)) \(String(describing: contactsTwo[0].birthday?.year))");
+            print("-------------------")
+            
+            // filter birthdays that have already passed
+            bday_contacts = bday_contacts.filter {
+                (($0.birthday?.month)! > calendar.component(.month, from: currentDate)) || (($0.birthday?.month)! == calendar.component(.month, from: currentDate) && ($0.birthday?.day)! >= calendar.component(.day, from: currentDate))
+            }
+            
+            for contact in bday_contacts {
+                print("oh boy here we go: \(String(describing: contact.familyName)) \(String(describing: contact.birthday?.year))");
+            }
+            //let fullNameTwo = CNContactFormatter.string(from: firstMatchTwo, style: .fullName);
+            
+            if bday_contacts.count > 0 {
+                let curr_contact = bday_contacts[0]
+                contactNameOne.text = CNContactFormatter.string(from: curr_contact, style: .fullName)
+                
+                birthdayOne.text = Months[(bday_contacts[0].birthday?.month)! - 1] + " \((bday_contacts[0].birthday?.day)!)"
+            }
+            
+            if bday_contacts.count > 1 {
+                let curr_contact = bday_contacts[1]
+                contactNameTwo.text = CNContactFormatter.string(from: curr_contact, style: .fullName)
+                birthdayTwo.text = Months[(bday_contacts[1].birthday?.month)! - 1] + " \((bday_contacts[1].birthday?.day)!)"
+            }
             
         } catch {
             print("Failed to fetch contact, error: \(error)");

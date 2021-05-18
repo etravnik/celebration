7891b329fc79cc4bcd331ac33cfda52a5bde4858
ckuse <officialconnorkuse@gmail.com>
2021-05-18 13:04:43 -0400

    notification framework updated, can receive/process

--- celebration/celebration/AppDelegate.swift
+++ celebration/celebration/AppDelegate.swift
@@ -44,7 +44,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func getNotificationSettings() {
         UNUserNotificationCenter.current().getNotificationSettings { settings in
             print("Notification Settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
         }
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+            let token = tokenParts.joined()
+            print("Device Token: \(token)")
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error ) {
+            print("Failed to register: \(error)")
     }
 
 }
